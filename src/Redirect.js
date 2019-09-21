9b215f002fdab0a80d58b8b3a6946b5259237ecd
Shu Dong <shudong@fb.com>
2019-09-20 20:43:26 -0700

    upgrade url service

--- src/Redirect.js
+++ src/Redirect.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom'
 import { withRouter } from "react-router";
-
-import { getUrl } from './getUrl'
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
+import { getUrl } from './getUrl';
 
 class ABCRedirect extends Component {
   constructor() {
@@ -10,10 +11,12 @@ class ABCRedirect extends Component {
     this.state = {
       longurl: "",
       error: "",
+      loading: true,
     }
   }
 
   componentDidMount() {
+    this.setState({loading: true});
     const shortUrlId = this.props.match.params.id;
     getUrl(shortUrlId)
       .then(longurl => {
@@ -21,6 +24,7 @@ class ABCRedirect extends Component {
       })
       .catch(err => {
         this.setState({
+          loading: false,
           error: err.message
         });
       });
@@ -28,14 +32,33 @@ class ABCRedirect extends Component {
 
   render() {
     return (
-      <div>
-        {
-          this.state.error ? (
-            <h2>{this.state.error}</h2>
-          ) : (
-            <h2>Welcome to join ABC Blockchain Community, redirecting...</h2>
-          )
-        }
+      <div className="container" style={{
+        display: 'flex',
+        justifyContent: 'center',
+        minHeight: 800
+      }}>
+        <div align='center' style={{
+          alignSelf: 'center'
+        }}>
+          <img src="/logo.png" style={{
+            maxWidth: 150,
+            maxHight: 150,
+          }} />
+          <Typography variant="h6" style={{
+            marginTop: 10,
+            marginBottom: 30
+          }}>
+            ABC Blockchain Community
+          </Typography>
+          <LinearProgress hidden={!this.state.loading} style={{
+            maxWidth: 200
+          }}/>
+          <Typography variant="subtitle1" style={{
+            marginTop: 10
+          }}>
+            {this.state.error ? this.state.error : "Redirecting..."}
+          </Typography>
+        </div>
       </div>
     );
   }
